206161cf12c35ffa4395318f058f7ac8811bc455
tfjackc <pnwest.geo@gmail.com>
2023-09-24 20:10:39 -0700

    working with react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'; // Import React and useState
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import { Icon } from 'leaflet';
+import axios from 'axios';
 import './App.css';
 
+// Define the GeoJSON URL
+const url = '/dgc_courses/?format=json';
+
+// Define a custom icon
+export const icon = new Icon({
+  iconUrl: 'blue-marker.png',
+});
+
 function App() {
+  // State to hold the GeoJSON data
+  const [geojsonData, setGeojsonData] = useState(null);
+
+  // Fetch GeoJSON data when the component mounts
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        const data = response.data;
+        setGeojsonData(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching GeoJSON data:', error);
+      });
+  }, []);
+
+  // Function to create a GeoJSON layer
+  // const createGeoJSONLayer = () => {
+  //   if (geojsonData) {
+  //     return (
+  //       <GeoJSON data={geojsonData} style={{ color: 'blue' }}>
+  //         {/* You can customize the style of the GeoJSON features here */}
+  //       </GeoJSON>
+  //     );
+  //   }
+  // };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Hello World</h1>
+        <h1>Disc Golf Courses</h1>
       </header>
+      <MapContainer center={[39.82, -98.58]} zoom={5} scrollWheelZoom={true}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <GeoJSON data={geojsonData} style={{ color: 'blue' }}>
+          {/* You can customize the style of the GeoJSON features here */}
+        </GeoJSON>
+      </MapContainer>
     </div>
   );
 }
